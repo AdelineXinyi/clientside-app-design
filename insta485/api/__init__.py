cdcd56795c63f7c8fdad9b591d41e91e263b571f
AdelineXinyi <89888667+AdelineXinyi@users.noreply.github.com>
2024-09-30 23:49:55 -0400

    Update __init__.py

--- insta485/api/__init__.py
+++ insta485/api/__init__.py
@@ -2,5 +2,6 @@
 
 from insta485.api.posts import get_post
 from insta485.api.posts import get_service
-from insta485.api.likes import add_like,delete_like
+from insta485.api.likes import add_like, delete_like
+from insta485.api.comments import add_comment, delete_comment
 
